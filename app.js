4ec943c51a4f9bebf12e6ca3e111f6908b670eb7
ohampton <44102431+ohampton@users.noreply.github.com>
2022-01-07 11:12:26 -0500

    commit

--- app.js
+++ app.js
@@ -3,18 +3,23 @@ const p2Button = document.querySelector('#p2Button')
 const resetButton = document.querySelector('#resetButton')
 const p1Display = document.querySelector('#p1Display')
 const p2Display = document.querySelector('#p2Display')
+const playTo = document.querySelector('#playTo')
 
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = 3;
 let isGameOver = false;
 
 p1Button.addEventListener('click', function (evt){
     if(!isGameOver) {
         p1Score += 1;
         if(p1Score === winningScore) {
-            isGameOver = true
+            isGameOver = true;
+            p1Display.classList.add('winner')
+            p2Display.classList.add('loser')
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p1Display.textContent = p1Score;
     }
@@ -24,9 +29,33 @@ p2Button.addEventListener('click', function (evt){
         p2Score += 1;
         if(p2Score === winningScore) {
             isGameOver = true
+            p2Display.classList.add('winner')
+            p1Display.classList.add('loser')
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p2Display.textContent = p2Score;
     }
 });
 
+playTo.addEventListener('change', function(){
+    winningScore =  parseInt(this.value);
+    reset();
+ });
+
+
+
+resetButton.addEventListener('click', reset )
+
+function reset(){
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove('winner', 'loser');
+    p2Display.classList.remove('winner', 'loser');
+    p1Button.disabled = false;
+    p2Button.disabled = false;
+}
 
